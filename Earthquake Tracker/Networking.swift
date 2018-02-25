ea9dd3b2ccefbc793b46c0e4df69fe60337f3eff
Zac Rose <zac@insightmobiledevelopment.com>
2018-02-24 21:25:55 -0500

    Pulling data from URL in JSON format successfully and parsing. Ready to extract the data needed and format into correct info for app.

--- Earthquake Tracker/Networking.swift
+++ Earthquake Tracker/Networking.swift
@@ -20,6 +20,37 @@ class Quake : NSObject {
         session = URLSession.shared
     }
     
+    // Get the data
+    func getQuakeData() {
+        let request = NSMutableURLRequest(url: URL(string: "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-01&endtime=2016-01-31&minmag=6&limit=10")!)
     
+    let task = URLSession.shared.dataTask(with: request as URLRequest) { (data, response, error) in
+        if error == nil {
+            
+            // Parse the data
+            if let data = data {
+                
+                let parsedResult: [String:AnyObject]!
+                do {
+                    parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:AnyObject]
+                } catch {
+                    print("Could not parse the data as JSON: '\(data)'")
+                    return
+                }
+                
+                print(parsedResult)
+            }
+        }
+    }
+    
+        task.resume()
+    }
+    
+    class func sharedInstance() -> Quake{
+        struct Singleton {
+            static var sharedInstance = Quake()
+        }
+        return Singleton.sharedInstance
+    }
     
 }
--- Earthquake Tracker/ViewController.swift
+++ Earthquake Tracker/ViewController.swift
@@ -12,8 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        Quake.sharedInstance().getQuakeData()   }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
